287de8ca4c7817cc2beb2677cf0b40842c6a55f0
selinduzel <selintuna92@gmail.com>
2022-09-26 14:17:00 +0300

    video 2 ve video 3

---
+++ resources/views/admin/data/footer.blade.php
@@ -0,0 +1,11 @@
+   <!--**********************************
+            Footer start
+        ***********************************-->
+        <div class="footer">
+            <div class="copyright">
+                <p>Copyright &copy; Tasarım <a href="https://phpturkiye.net">PHP Türkiye</a> 2021</p>
+            </div>
+        </div>
+        <!--**********************************
+            Footer end
+        ***********************************-->
--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,14 @@ Route::get('/', function () {
 
 
 Route::get('/yonetim', function () {
-    return view('admin.tema');
+    return view('admin.include.home');
+});
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
